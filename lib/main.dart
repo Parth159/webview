98ad07b756c9c12a9b8581bdde28482710f635b7
silverxis <sivakumar@silverxis.com>
2022-03-14 10:58:23 +0530

    double splash new logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
-import 'package:webview/splash/splash_screen.dart';
+
+import 'webview/webview.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void removeSplash() {
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
+    removeSplash();
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'PAS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const IntroScreen(),
+      home: WebViewLoad(),
     );
   }
 }
--- lib/webview/webview.dart
+++ lib/webview/webview.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewLoad extends StatelessWidget {
-  const WebViewLoad({Key? key}) : super(key: key);
+  final showWidget = CrossFadeState.showFirst.obs;
+  WebViewLoad({Key? key}) : super(key: key);
+
+  void onPageLoaded() {
+    showWidget.value = CrossFadeState.showSecond;
+  }
+
+  Widget webView() {
+    return WebView(
+      onPageFinished: (value) {
+        onPageLoaded();
+      },
+      initialUrl: "http://13.59.197.11/",
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+
+  Widget animatedSplash() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/gif/intro_splash.gif"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: WebView(
-          initialUrl: "http://13.59.197.11/",
-          javascriptMode: JavascriptMode.unrestricted,
+        body: Obx(
+          () => AnimatedCrossFade(
+            duration: const Duration(milliseconds: 500),
+            crossFadeState: showWidget.value,
+            firstChild: animatedSplash(),
+            secondChild: webView(),
+          ),
         ),
       ),
     );
